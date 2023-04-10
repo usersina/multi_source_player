4fb89393a998d6f6b8e724ac51666854ded7c37c
usersina <benna.anis2@gmail.com>
2023-04-10 18:22:07 +0100

    Implement dual playing functionality

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -19,7 +19,8 @@ class MainScreen extends StatelessWidget {
               MaterialPageRoute(
                 builder: (context) => const MultiSourcePlayerScreen(
                   videoUrl:
-                      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+                      // 'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+                      'https://storage.googleapis.com/kini_static/exercise/21597cb0/3da0/4b35/90b6/4b7076bb77fe/138c5ab2-bf72-4cfe-ab6d-f73b85eff8ab.mp4',
                   audioUrl:
                       'https://storage.googleapis.com/kini_static/exercise/78debd09/3138/4abd/9536/60a5384689f3/92f795ac-ed8f-41be-8293-67a231257747.mp3',
                 ),
--- lib/screens/multi_source_player_screen.dart
+++ lib/screens/multi_source_player_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:developer';
-
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:video_player/video_player.dart';
 
 class MultiSourcePlayerScreen extends StatefulWidget {
@@ -18,29 +17,61 @@ class MultiSourcePlayerScreen extends StatefulWidget {
 }
 
 class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
-  late VideoPlayerController _controller;
+  late VideoPlayerController _videoController;
+  final AudioPlayer _audioPlayer = AudioPlayer();
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(
+    _initVideoPlayer();
+    _initAudioPlayer();
+  }
+
+  Future<void> _initVideoPlayer() async {
+    _videoController = VideoPlayerController.network(
       widget.videoUrl,
       videoPlayerOptions: VideoPlayerOptions(
-        mixWithOthers: false,
+        mixWithOthers: true,
       ),
-    )..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
+    );
+    await _videoController.initialize();
+    _videoController.setVolume(0);
 
-        _controller.setVolume(0);
-        _controller.addListener(() {
-          // Needed so that play/pause button and custom progress bar are updated
-          if (!mounted) {
-            return;
-          }
-          setState(() {});
-        });
-      });
+    // Needed so that play/pause button and custom progress bar are updated
+    _videoController.addListener(() {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+
+    // Pause audio player when video is buffering
+    _videoController.addListener(() {
+      if (_videoController.value.isBuffering) {
+        _audioPlayer.pause();
+      } else if (_videoController.value.isPlaying) {
+        _audioPlayer.play();
+      }
+    });
+
+    // Stop audio once video is finished
+    _videoController.addListener(() {
+      if (_videoController.value.position == _videoController.value.duration) {
+        _audioPlayer.setLoopMode(LoopMode.off);
+        _audioPlayer.stop();
+      } else {
+        _audioPlayer.setLoopMode(LoopMode.one);
+      }
+    });
+
+    // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+    setState(() {});
+  }
+
+  Future<void> _initAudioPlayer() async {
+    await _audioPlayer.setUrl(widget.audioUrl);
+    await _audioPlayer.setVolume(1);
+    _audioPlayer.setLoopMode(LoopMode.one);
   }
 
   @override
@@ -53,10 +84,10 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
         children: [
           const Spacer(),
           Center(
-            child: _controller.value.isInitialized
+            child: _videoController.value.isInitialized
                 ? AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
+                    aspectRatio: _videoController.value.aspectRatio,
+                    child: VideoPlayer(_videoController),
                   )
                 : Container(),
           ),
@@ -64,17 +95,17 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
             padding: const EdgeInsets.all(8.0),
             // // Default progress bar
             // child: VideoProgressIndicator(
-            //   _controller,
+            //   _videoController,
             //   allowScrubbing: true,
             // ),
             // Custom progress bar
             child: StreamBuilder<DurationState>(
-              stream: _controller.position.asStream().map(
+              stream: _videoController.position.asStream().map(
                 (progress) {
                   return DurationState(
                     progress: progress ?? Duration.zero,
-                    buffered: _controller.value.buffered.last.end,
-                    total: _controller.value.duration,
+                    buffered: _videoController.value.buffered.last.end,
+                    total: _videoController.value.duration,
                   );
                 },
               ),
@@ -88,7 +119,7 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
                   buffered: buffered,
                   total: total,
                   onSeek: (duration) {
-                    _controller.seekTo(duration);
+                    _videoController.seekTo(duration);
                   },
                 );
               },
@@ -96,12 +127,27 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
           ), // Video ProgressBar
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ProgressBar(
-              progress: const Duration(milliseconds: 1000),
-              buffered: const Duration(milliseconds: 2000),
-              total: const Duration(milliseconds: 5000),
-              onSeek: (duration) {
-                log('User selected a new time: $duration');
+            child: StreamBuilder<DurationState>(
+              stream: _audioPlayer.positionStream.map(
+                (progress) => DurationState(
+                  progress: progress,
+                  buffered: _audioPlayer.bufferedPosition,
+                  total: _audioPlayer.duration ?? Duration.zero,
+                ),
+              ),
+              builder: (context, snapshot) {
+                final durationState = snapshot.data;
+                final progress = durationState?.progress ?? Duration.zero;
+                final buffered = durationState?.buffered ?? Duration.zero;
+                final total = durationState?.total ?? Duration.zero;
+                return ProgressBar(
+                  progress: progress,
+                  buffered: buffered,
+                  total: total,
+                  onSeek: (duration) {
+                    _audioPlayer.seek(duration);
+                  },
+                );
               },
             ), // Audio ProgressBar
           ),
@@ -110,14 +156,19 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          if (!_videoController.value.isInitialized) return;
           setState(() {
-            _controller.value.isPlaying
-                ? _controller.pause()
-                : _controller.play();
+            if (_videoController.value.isPlaying) {
+              _videoController.pause();
+              _audioPlayer.pause();
+            } else {
+              _videoController.play();
+              _audioPlayer.play();
+            }
           });
         },
         child: Icon(
-          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+          _videoController.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
@@ -126,7 +177,8 @@ class _MultiSourcePlayerScreenState extends State<MultiSourcePlayerScreen> {
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    _videoController.dispose();
+    _audioPlayer.dispose();
   }
 }
 
