c9ae1383a3984ebeebd2b852410aa6cd6db0dc9b
usersina <benna.anis2@gmail.com>
2023-04-10 17:07:15 +0100

    Add packages and separate screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:multi_source_player/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,29 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My App'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('Play Video'),
-            onPressed: () {
-              // Navigator.push(
-              //   context,
-              //   MaterialPageRoute(
-              //     builder: (context) => const VideoPlayerScreen(
-              //       videoUrl:
-              //           'https://storage.googleapis.com/kini_static/exercise/21597cb0/3da0/4b35/90b6/4b7076bb77fe/138c5ab2-bf72-4cfe-ab6d-f73b85eff8ab.mp4',
-              //       audioUrl:
-              //           'https://storage.googleapis.com/kini_static/exercise/78debd09/3138/4abd/9536/60a5384689f3/92f795ac-ed8f-41be-8293-67a231257747.mp3',
-              //     ),
-              //   ),
-              // );
-            },
-          ),
-        ),
-      ),
+      home: const MainScreen(),
     );
   }
 }
